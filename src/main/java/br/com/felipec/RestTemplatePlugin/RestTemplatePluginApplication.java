3b0206819f1602ec1cd33e0048dfc41467a8f025
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-26 23:14:08 -0300

    feat: initial commit

---
+++ src/main/java/br/com/felipec/RestTemplatePlugin/RestTemplatePluginApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipec.RestTemplatePlugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplatePluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplatePluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipec/RestTemplatePlugin/RestTemplatePluginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipec.RestTemplatePlugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplatePluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
